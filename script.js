e4360f093ea96ddb8aa82365ebff766fa6d52de1
Eric Posas <web_dev@tutanota.com>
2019-05-17 13:13:18 -0500

    updated function to generate over 60,000 names

--- script.js
+++ script.js
@@ -20,15 +20,9 @@ if (program.outfile){
       const arr = data.split('\r\n')
       const newArr = []
       // make some combinations with the original list
-      arr.forEach((item, i) => { if (i < 250) newArr.push([`${item} ${arr[i+1]}`]) })
-      arr.forEach((item, i) => { if (i < 240) newArr.push([`${item} ${arr[i+10]}`]) })
-      arr.forEach((item, i) => { if (i < 220) newArr.push([`${item} ${arr[i+20]}`]) })
-      arr.forEach((item, i) => { if (i < 200) newArr.push([`${item} ${arr[i+40]}`]) })
-      arr.forEach((item, i) => { if (i < 180) newArr.push([`${item} ${arr[i+60]}`]) })
-      arr.forEach((item, i) => { if (i < 160) newArr.push([`${item} ${arr[i+80]}`]) })
-      arr.forEach((item, i) => { if (i < 140) newArr.push([`${item} ${arr[i+100]}`]) })
-      arr.forEach((item, i) => { if (i < 120) newArr.push([`${item} ${arr[i+120]}`]) })
-      arr.forEach((item, i) => { if (i < 90) newArr.push([`${item} ${arr[i+150]}`]) })
+      for (let i = 0; i < arr.length-1; i++){
+        arr.forEach((item) => { newArr.push([`${item} ${arr[i]}`]) })
+      }
       // join them as line separated values
       let list = newArr.join('\r\n')
       // write them to .csv
